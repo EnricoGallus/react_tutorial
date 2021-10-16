0acfe7cf61d34dfd0b91249d8d915cc5b6e6da4f
Enrico Gallus <enrico.gallus@gmail.com>
2021-10-17 05:45:44 +0900

    jsx css

--- src/index.js
+++ src/index.js
@@ -1,37 +1,33 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// stateless functional component
-// always return jsx
+import './index.css'
 
-// JSX rules
-// return single element
-// div / section / article or Fragment
-// use camelCase for html of class
-// className instead of class
-// close every element
-// formatting
-
-// Nested Components
-
-function Greeting() {
+function BookList() {
     return (
-        <div>
-            <Person />
-            <Message />
-        </div>);
+        <section className="booklist">
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>);
 }
 
-// implicit return
-const Person = () => <h2>john doe</h2>;
-
-// explicit return
-const Message = () => {
-    return <p>this is my message</p>
+const Book = () => {
+    return <article className="book">
+        <Image />
+        <Title />
+        <Author />
+    </article>
 }
 
-/*const Greeting = () => {
-    return React.createElement('div', {}, React.createElement('h1', {}, 'hello world'));
-}*/
+const Image = () =>
+    (<img src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg" alt="" />);
 
-ReactDom.render(<Greeting/>, document.getElementById('root'));
+const Title = () => <h1>I Love You to the Moon and Back</h1>
+const Author = () => <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Amelia Hepworth</h4>
+
+ReactDom.render(<BookList/>, document.getElementById('root'));
